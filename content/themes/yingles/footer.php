531a13ba4cf718eff850a5540bc13309dbbe3511
Jason Yingling <yingling017@gmail.com>
2014-03-12 08:40:05 -0500

    Installed theme

---
+++ content/themes/yingles/footer.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * @package boiler
+ */
+?>
+
+			<footer id="globalFooter" class="site_footer">
+				<p class="copy">&copy; <?php echo date('Y'); ?> <?php bloginfo('site_title'); ?></p>
+			</footer>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
